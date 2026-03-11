aa47f75b93e67f77cdbac043db1fd244de240a67
Sebastian Restrepo Rico <84477914+srestre84@users.noreply.github.com>
2026-03-11 14:22:10 -0500

    fix: add CNAME generation and set root base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/suconsultorfinanciero.online/', // Añadido para GitHub Pages
+    base: '/', // Raíz del dominio personalizado
 })
